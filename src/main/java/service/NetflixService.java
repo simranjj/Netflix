028958c06291f3b4aace095083baee098d68a1a3
simranjj <simranjeet271@gmail.com>
2020-07-31 09:47:43 -0400

    Changes to service name

--- src/main/java/service/NetflixService.java
+++ src/main/java/service/NetflixService.java
@@ -21,7 +21,7 @@ import model.Series;
  * @author simra
  */
 @MTOM(enabled = true, threshold = 0)
-@WebService
+@WebService(serviceName = "NetflixService")
 @HandlerChain(file = "NetflixServiceService_handler.xml")
 public class NetflixService {
 
